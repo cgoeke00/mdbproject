059e38126be44055ed7646c34febb59e9cb5a452
Chandler Goeke <cgoeke00@gmail.com>
2023-12-04 16:23:55 -0600

    Added Neo4j node population and relationships.

--- populate.py
+++ populate.py
@@ -1,11 +1,18 @@
 import redis
+from neo4j import GraphDatabase
 import csv
 import json
 
 # Redis client
 redisClient = redis.Redis(port=6379);
+redisClient.flushdb()
 
-# The CSV file containing the pokemon data
+# Neo client
+neo4jUrl = "bolt://localhost:7687"
+neo4jDriver = GraphDatabase.driver(neo4jUrl, auth=('neo4j', 'password'))
+session = neo4jDriver.session()
+
+# The CSV file containing the profile data
 csvFilename = "data.csv"
 # Track how many file lines we've processed
 processedLines = 0
@@ -34,19 +41,53 @@ def populateRedis():
                 "tags": tags,
                 "pfp": pfp,
                 "followersCount": followersCount,
-                "followers": followers
+                "followers": clean_strings(followers)
             }
     
             line_count += 1
             if line_count%100 == 0:
                 print(f'Processed {line_count} lines.')
-                return ################################## only do first 100
-            ############################################# until format is finalized
 
+            # Populate Redis
             redisClient.sadd(id, json.dumps(profileObj))
+
+            # Create Neo nodes
+            session.run('CREATE (u:User {id: ' + id + '})')
+
 
     print(f'Processed {line_count} lines.')
 
+# Add follow relationships to Neo nodes
+def addRelationships():
+    rel_count = 0;
+    with open(csvFilename, encoding="utf8") as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for data in csv_reader:
+
+            id = data['id'];
+            followers = data['friends']
+            if followers != '[]':
+                followers = data['friends'].split('[ ') + data[None]
+
+            followers = clean_strings(followers)
+
+            for follower in followers:
+                # Create Neo relationships
+                session.run('MATCH (u1:User {id: ' + id + '}) MATCH (u2:User {id: ' + follower + '}) MERGE (u1)-[:FOLLOWS]->(u2)')
+                rel_count += 1
+
+                if rel_count%100 == 0:
+                    print(f'Added {rel_count} Relationships.')
+
+    print(f'Added {rel_count} relationships.')
+
+
+def clean_strings(string_list):
+    # Remove unwanted characters and filter out empty strings
+    cleaned_list = [s.replace('"', '').replace(']', '').replace(' ', '') for s in string_list if s != '']
+    return cleaned_list
+
 
 populateRedis();
+addRelationships();
 redisClient.quit();
