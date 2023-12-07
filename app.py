91a08d4cbdca057b39da2937fda39a3162058cb5
Tommy Subaric <91640793+tsubaric@users.noreply.github.com>
2023-12-06 20:31:41 -0600

    Neo4j Similar Users Query added

--- app.py
+++ app.py
@@ -16,13 +16,29 @@ def home():
 @app.route('/test')
 def test():
     user_id = 16190898  # Replace with the actual user ID
-    data = Neo.query(f'MATCH (specificUser:User {{id: {user_id}}})-[:FOLLOWS]->(follower:User), '
+    result = Neo.query(f'MATCH (specificUser:User {{id: {user_id}}})-[:FOLLOWS]->(follower:User), '
                     '(follower)-[:FOLLOWS]->(commonUser:User) '
                     'WHERE specificUser <> commonUser '
                     'AND NOT (specificUser)-[:FOLLOWS]->(commonUser) '
                     'RETURN DISTINCT commonUser.id '
                     'LIMIT 5')
-    return render_template('test.html', data=data, user_id=user_id)
+    return render_template('test.html', data=result, user_id=user_id)
+
+@app.route('/test1')
+def test1():
+    # Step 3 and 4: Cypher Query
+    user_id = 16190898  # Replace with the actual user ID
+    query = f"""
+        MATCH (source:User {{id: {user_id}}})-[:FOLLOWS]->(follower:User)
+        WITH source, COLLECT(follower) AS followers
+        MATCH (source)-[similarity:SIMILARITY]-(similarUser:User)
+        RETURN similarUser.id AS similarUserId, similarity.cosineSimilarity AS similarityPercentage
+        ORDER BY similarityPercentage DESC
+        LIMIT 5;
+    """
+    result = Neo.query(query)
+
+    return render_template('test1.html', data=result, user_id=user_id)
 
 if __name__ == '__main__':
     app.run(debug=True)
